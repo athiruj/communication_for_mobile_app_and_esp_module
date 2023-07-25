4825ad390726c097a100272aa46548f16ed85e08
athiruj.k <athirujkaewseesuk@gmail.com>
2023-07-25 23:51:28 +0700

    ContainerBarStyle classes

--- lib/components/container_bar.dart
+++ lib/components/container_bar.dart
@@ -82,6 +82,69 @@ class _ContainerBarState extends State<ContainerBar>
       return widgetValue ?? defaultValue;
     }
 
+    // T? resolve<T>(
+    //     MaterialStateProperty<T>? Function(ContainerBarStyle? style)
+    //         getProperty) {
+    //   return effectiveValue(
+    //     (ContainerBarStyle? style) {
+    //       return getProperty(style)?.resolve(statesController.value);
+    //     },
+    //   );
+    // }
+    final Color? resolvedBackgroundColor =
+        effectiveValue((ContainerBarStyle? style) => style?.backgroundColor);
+    final Color? resolvedForegroundColor =
+        effectiveValue((ContainerBarStyle? style) => style?.foregroundColor);
+    final Size? resolvedMinimumSize =
+        effectiveValue((ContainerBarStyle? style) => style?.minimumSize);
+    final Size? resolvedFixedSize =
+        effectiveValue((ContainerBarStyle? style) => style?.fixedSize);
+    final Size? resolvedMaximumSize =
+        effectiveValue((ContainerBarStyle? style) => style?.maximumSize);
+    final double? resolvedMinimumFactor =
+        effectiveValue((ContainerBarStyle? style) => style?.minimumFactor);
+    final double? resolvedFixedFactor =
+        effectiveValue((ContainerBarStyle? style) => style?.fixedFactor);
+    final double? resolvedMaximumFactor =
+        effectiveValue((ContainerBarStyle? style) => style?.maximumFactor);
+
+    final VisualDensity? resolvedVisualDensity =
+        effectiveValue((ContainerBarStyle? style) => style?.visualDensity);
+    final EdgeInsetsGeometry? resolvedPadding =
+        effectiveValue((ContainerBarStyle? style) => style?.padding);
+    final OutlinedBorder? resolveShape =
+        effectiveValue((ContainerBarStyle? style) => style?.shape);
+    final Duration? resolvedAnimationDuration =
+        effectiveValue((ContainerBarStyle? style) => style?.animationDuration);
+    final AlignmentGeometry? resolveAlignment =
+        effectiveValue((ContainerBarStyle? style) => style?.alignment);
+    final Offset densityAdjustment = resolvedVisualDensity!.baseSizeAdjustment;
+
+    BoxConstraints effectiveConstraints =
+        resolvedVisualDensity.effectiveConstraints(
+      BoxConstraints(
+        minWidth: resolvedMinimumSize!.width,
+        minHeight: resolvedMinimumSize.height,
+        maxWidth: resolvedMaximumSize!.width,
+        maxHeight: resolvedMaximumSize.height,
+      ),
+    );
+    if (resolvedFixedSize != null) {
+      final Size size = effectiveConstraints.constrain(resolvedFixedSize);
+      if (size.width.isFinite) {
+        effectiveConstraints = effectiveConstraints.copyWith(
+          minWidth: size.width,
+          maxWidth: size.width,
+        );
+      }
+      if (size.height.isFinite) {
+        effectiveConstraints = effectiveConstraints.copyWith(
+          minHeight: size.height,
+          maxHeight: size.height,
+        );
+      }
+    }
+
     final Widget result = ConstrainedBox(constraints: const BoxConstraints());
     return Container();
   }
@@ -92,44 +155,60 @@ class ContainerBarStyle with Diagnosticable {
     this.backgroundColor,
     this.foregroundColor,
     this.minimumSize,
+    this.fixedSize,
     this.maximumSize,
     this.minimumFactor,
+    this.fixedFactor,
     this.maximumFactor,
+    this.visualDensity,
     this.padding,
     this.shape,
+    this.animationDuration,
     this.alignment,
   });
 
   final Color? backgroundColor;
   final Color? foregroundColor;
   final Size? minimumSize;
+  final Size? fixedSize;
   final Size? maximumSize;
   final double? minimumFactor;
+  final double? fixedFactor;
   final double? maximumFactor;
+  final VisualDensity? visualDensity;
   final EdgeInsetsGeometry? padding;
   final OutlinedBorder? shape;
+  final Duration? animationDuration;
   final AlignmentGeometry? alignment;
 
   ContainerBarStyle copyWith({
     Color? backgroundColor,
     Color? foregroundColor,
     Size? minimumSize,
+    Size? fixedSize,
     Size? maximumSize,
     double? minimumFactor,
+    double? fixedFactor,
     double? maximumFactor,
+    VisualDensity? visualDensity,
     EdgeInsetsGeometry? padding,
     OutlinedBorder? shape,
+    Duration? animationDuration,
     AlignmentGeometry? alignment,
   }) {
     return ContainerBarStyle(
       backgroundColor: backgroundColor ?? this.backgroundColor,
       foregroundColor: foregroundColor ?? this.foregroundColor,
       minimumSize: minimumSize ?? this.minimumSize,
+      fixedSize: fixedSize ?? this.fixedSize,
       maximumSize: maximumSize ?? this.maximumSize,
       minimumFactor: minimumFactor ?? this.minimumFactor,
+      fixedFactor: fixedFactor ?? this.fixedFactor,
       maximumFactor: maximumFactor ?? this.maximumFactor,
+      visualDensity: visualDensity ?? this.visualDensity,      
       padding: padding ?? this.padding,
       shape: shape ?? this.shape,
+      animationDuration: animationDuration ?? this.animationDuration,
       alignment: alignment ?? this.alignment,
     );
   }
